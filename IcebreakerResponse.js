b701b2a090a9e750162927e1d72580fbb780024f
Angelo Cigna <acigna1982@gmail.com>
2017-11-14 19:54:59 +0000

    automatically backed up by learn

--- IcebreakerResponse.js
+++ IcebreakerResponse.js
@@ -2,12 +2,20 @@ function IcebreakerResponse(email){
   this.email = email
 }
 
-Icebreaker.BatchCreate = function(data){
-  return data.map(function(IcebreakerResponseData) {
-    return new IcebreakerResponse(IcebreakerResponseData[0], IcebreakerResponseData [1])
+Icebreaker.BatchCreate = function(emails){
+  let icebreakers = []
+  emails.forEach(function(email){
+    icebreakers.push(new IcebreakerResponse(email))
   })
 
+  return icebreakers
 }
+  
+//   return data.map(function(IcebreakerResponseData) {
+//     return new IcebreakerResponse(IcebreakerResponseData[0], IcebreakerResponseData [1])
+//   })
+// 
+// }
 
 // // return emails.map(function(email){
 // //   return new IcebreakerResponse(email)
